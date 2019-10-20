d4250dcad27b0eaac733e9094d6039bea7ee1116
Anujit Nene <anujitnene@gmail.com>
2019-10-20 19:40:42 +0530

    Very dumb auth containers

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import IsAuthenticated from '../containers/IsAuthenticated';
 
 export default class LandingPage extends React.Component {
     render() {
-        return <div>Landing Page</div>;
+        return (
+            <IsAuthenticated>
+                <div>Landing Page Authenticated!!!</div>
+            </IsAuthenticated>
+        );
     }
 }
-
